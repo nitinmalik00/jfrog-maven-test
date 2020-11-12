9b33894ae537a27e4569c747d88f2d1bc7e3ee88
Akshay Bhatt <akshay.bhatt@flipkart.com>
2020-11-12 13:19:43 +0530

    Build

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello V2, %s!";
+	private static final String template = "Hello V3, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
