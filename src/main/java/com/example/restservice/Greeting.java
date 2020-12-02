dd9eaba716024ae098bb0ecc70409e325b997b17
Akshay Bhatt <akshay.bhatt@flipkart.com>
2020-12-02 18:39:27 +0530

    Build this pipeline

--- src/main/java/com/example/restservice/Greeting.java
+++ src/main/java/com/example/restservice/Greeting.java
@@ -4,10 +4,12 @@ public class Greeting {
 //
 	private final long id;
 	private final String content;
+	private final String staticConfig;
 
-	public Greeting(long id, String content) {
+	public Greeting(long id, String content, String staticConfig) {
 		this.id = id;
 		this.content = content;
+		this.staticConfig = staticConfig;
 	}
 
 	public long getId() {
@@ -16,5 +18,18 @@ public class Greeting {
 
 	public String getContent() {
 		return content;
+	}
+
+	public String getStaticConfig() {
+		return staticConfig;
+	}
+
+	@Override
+	public String toString() {
+		return "Greeting{" +
+				"id=" + id +
+				", content='" + content + '\'' +
+				", staticConfig='" + staticConfig + '\'' +
+				'}';
 	}
 }
--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -2,18 +2,26 @@ package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@PropertySource("file:/Users/akshay.bhatt/Documents/flipkart/fk-build-flow-demo/src/main/java/resources/application.properties")
+@PropertySource("file:${SERVER_CONFIG}")
 public class GreetingController {
 
 	private static final String template = "Hello V24, %s!";
+
+	@Value("${config.parameter}")
+	private String configParam;
+
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		return new Greeting(counter.incrementAndGet(), String.format(template, name), configParam);
 	}
 }
