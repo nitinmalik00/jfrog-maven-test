a18569ff4ff452d90e0dfffc556d2a3896c375e1
vaibhav.dubey <vaibhav.dubey@flipkart.com>
2020-12-03 19:48:03 +0530

    Stage 2 (#68)
    * Update GreetingControllerTests.java
    
    xyz
    
    * Update GreetingControllerTests.java
    
    changes

--- src/test/java/com/example/restservice/GreetingControllerTests.java
+++ src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -35,7 +35,7 @@ public class GreetingControllerTests {
 	@Autowired
 	private MockMvc mockMvc;
 
-	//@Test
+	@Test
 	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
 		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
