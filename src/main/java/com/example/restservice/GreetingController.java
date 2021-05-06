86d0209ebf94d732e19987f9a741f1f117f75e86
Akshay Bhatt <akshay.bhatt@flipkart.com>
2021-05-06 12:14:26 +0530

    Build

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello V28, %s!";
+	private static final String template = "Hello V29, %s!";
 
 	@Value("${config.parameter}")
 	private String configParam;
