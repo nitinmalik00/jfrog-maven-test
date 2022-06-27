a57f0da69bd97880518bdd1e8a3d995caadbbbb6
Shivam Maurya <shivam.maurya@flipkart.com>
2022-06-27 17:53:59 +0530

    added new class

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,18 +1,87 @@
 package com.example.restservice;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @Configuration
 public class RestServiceApplication {
 
     public static void main(String[] args) {
+
+        RestServiceApplication tester = new RestServiceApplication();
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+
+            Map<String,Object> studentDataMap = new HashMap<String,Object>();
+            int[] marks = {1,2,3};
+
+            Student student = new Student();
+            student.setAge(10);
+            student.setName("Mahesh");
+            // JAVA Object
+            studentDataMap.put("student", student);
+            // JAVA String
+            studentDataMap.put("name", "Mahesh Kumar");
+            // JAVA Boolean
+            studentDataMap.put("verified", Boolean.FALSE);
+            // Array
+            studentDataMap.put("marks", marks);
+
+            mapper.writeValue(new File("student.json"), studentDataMap);
+            //result student.json
+            //{
+            //   "student":{"name":"Mahesh","age":10},
+            //   "marks":[1,2,3],
+            //   "verified":false,
+            //   "name":"Mahesh Kumar"
+            //}
+            studentDataMap = mapper.readValue(new File("student.json"), Map.class);
+
+            System.out.println(studentDataMap.get("student"));
+            System.out.println(studentDataMap.get("name"));
+            System.out.println(studentDataMap.get("verified"));
+            System.out.println(studentDataMap.get("marks"));
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         SpringApplication.run(RestServiceApplication.class, args);
     }
 
 
+}
+
+class Student {
+    private String name;
+    private int age;
+    public Student(){}
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String toString(){
+        return "Student [ name: "+name+", age: "+ age+ " ]";
+    }
 }
